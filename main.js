e4a36cd89ce6d339579127f0262d3a8d1e3e14f6
Anastasia <treneranastasia@gmail.com>
2020-10-28 23:59:49 +0300

    Add crash

--- main.js
+++ main.js
@@ -116,7 +116,19 @@ function moveRoad() {
 
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
+
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item. getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+            settings.start = false;
+            console.warn('ДТП');
+        }
+
         item.y += settings.speed / 2;
         item.style.top = item.y + 'px';
         if(item.y >= document.documentElement.clientHeight) {
